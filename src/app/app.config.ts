a8bc9ed5af156b41d62ab2f9ec9865ddfa15497e
PanTs <pants.ath@gmail.com>
2026-07-15 18:53:03 +0300

    feat: add material design and bootstrap

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
