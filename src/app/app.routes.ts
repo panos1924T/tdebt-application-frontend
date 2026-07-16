9d34e44d096dee619b292c73071efe241be3001d
PanTs <pants.ath@gmail.com>
2026-07-16 14:14:31 +0300

    feat: set routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./features/auth/login/login').then(m => m.LoginComponent) },
+  { path: 'register', loadComponent: () => import('./features/auth/register/register').then(m => m.RegisterComponent) },
+  {
+    path: '',
+    canActivate: [authGuard],
+    loadComponent: () => import('./shared/components/layout/layout').then(m => m.LayoutComponent),
+    children: [
+      { path: 'dashboard', loadComponent: () => import('./features/dashboard/dashboard').then(m => m.DashboardComponent) },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'debts', loadComponent: () => import('./features/debts/debt-list/debt-list').then(m => m.DebtListComponent) },
+    ]
+  },
+  { path: '**', redirectTo: 'login' }
+];
